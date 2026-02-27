e3abb4dadf78236d6113d215654088acebcf3551
Harun <harun708280@gmail.com>
2026-02-27 10:59:40 +0600

    first commit

--- dist/src/app.js
+++ dist/src/app.js
@@ -16,6 +16,7 @@ app.use((0, cors_1.default)({
     credentials: true,
 }));
 // kf
+
 app.use(express_1.default.json({ limit: "10mb" }));
 app.use((0, cookie_parser_1.default)());
 app.use(express_1.default.urlencoded({ extended: true, limit: "10mb" }));

82cdd302ac8bb542a90efb83a30ab107f07cf114
Harun <harun708280@gmail.com>
2026-03-02 02:07:21 +0600

    refactor: Convert Prisma configuration from TypeScript to JavaScript CommonJS.

--- prisma.config.js
+++ prisma.config.js
@@ -1,19 +1,16 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const dotenv_1 = __importDefault(require("dotenv"));
-const node_path_1 = __importDefault(require("node:path"));
-const config_1 = require("prisma/config");
-dotenv_1.default.config();
-exports.default = (0, config_1.defineConfig)({
-    schema: node_path_1.default.join("prisma/schema"),
+const { defineConfig, env } = require("prisma/config");
+const path = require("node:path");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+module.exports = defineConfig({
+    schema: path.join("prisma/schema"),
     migrations: {
         path: "prisma/migrations",
     },
     engine: "classic",
     datasource: {
-        url: (0, config_1.env)("DATABASE_URL"),
+        url: env("DATABASE_URL"),
     },
 });

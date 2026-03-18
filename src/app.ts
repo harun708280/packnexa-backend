41ecd0a99c7915fe4b85373daffabb4a7cacf86f
Harun <harun708280@gmail.com>
2026-03-18 12:37:21 +0600

    chore: update CORS origin by replacing packnexa-by-harun.netlify.app with packnexa.netlify.app

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ import { envVariables } from "./config";
 const app: Application = express();
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:5173", "https://packnexa-frontend.vercel.app", "https://packnexa-by-harun.netlify.app", "https://packnexa.com"],
+    origin: ["http://localhost:3000", "http://localhost:5173", "https://packnexa-frontend.vercel.app", "https://packnexa.netlify.app", "https://packnexa.com",],
     credentials: true,
   })
 );
